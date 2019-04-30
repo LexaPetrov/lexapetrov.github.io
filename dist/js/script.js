641ac7e6b8461ae64cad91ba5fba6f934200cf10
Alexey <al1111997@yandex.ru>
2019-04-30 13:36:08 +0500

    'commit'

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,14 @@ var show = document.getElementById("showContent");
 			content.style.display = "block";
 	})
 });
+
+  $(document).ready(function(){
+    $("a[href*=#]").on("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+        e.preventDefault();
+        return false;
+    });
+});
