0ecdbe59ccdfc0e9ecc88c5986f31f519775204c
Алексей Петров <al1111997@yandex.ru>
2019-11-26 12:28:05 +0500

    Added some stuff

---
+++ data.js
@@ -0,0 +1,84 @@
+let data = [
+  {
+    "id": 0,
+    "img": "./dist/img/donate.jpg",
+    "title": "Поддержать копеечкой",
+    "link": "https://rocketbank.ru/aleksey-petrov-sparkling-hill",
+    "description": `Здесь можно отправить мне деньги на карту, они мне
+    помогут развиваться в творчестве`,
+    "date": "∞",
+    "tags": "#помощь #рокетбанк #донат"
+  },
+  {
+    "id": 1,
+    "img": "./dist/img/mycompany_result.jpg",
+    "title": "PSD-макет 'mycompany'",
+    "link": "pages/mycompany/",
+    "description": "Вёрстка макета из Интернета.",
+    "date": "сен 2017",
+    "tags": "#photoshop #html #css"
+  },
+  {
+    "id": 2,
+    "img": "./dist/img/modern_result.jpg",
+    "title": "PSD-макет 'modern'",
+    "link": "pages/modern/",
+    "description": "Вёрстка макета из Интернета.",
+    "date": "янв 2018",
+    "tags": "#photoshop #html #css"
+  },
+  {
+    "id": 3,
+    "img": "./dist/img/kontrcassa_result.jpg",
+    "title": "PSD-макет 'контркасса'",
+    "link": "pages/kontrcassa/",
+    "description": "Вёрстка макета для компании 'Web-Строитель'",
+    "date": "апр 2018",
+    "tags": "#photoshop #html #css"
+  },
+  {
+    "id": 4,
+    "img": "./dist/img/educationtest_result.png",
+    "title": "Веб-приложение 'тест по ИБ'",
+    "link": "pages/educationtest/",
+    "description": "Тест для учащихся школ и вузов в области информационной безопасности. Реализовано без сервера, но есть возможность сохранять результат в специальном коде.",
+    "date": "май 2019",
+    "tags": "#html #css #javascript #gh-pages"
+  },
+  {
+    "id": 5,
+    "img": "./dist/img/redbox_result.jpg",
+    "title": "PSD-макет 'redbox'",
+    "link": "pages/redbox/",
+    "description": "Вёрстка макета для компании Microimpuls.",
+    "date": "июл 2019",
+    "tags": "#photoshop #html #css"
+  },
+  {
+    "id": 6,
+    "img": "./dist/img/takeme_result.jpg",
+    "title": "PSD-макет 'take me'",
+    "link": "pages/takeme/",
+    "description": "Вёрстка макета из Интернета.",
+    "date": "авг 2019",
+    "tags": "#photoshop #html #css"
+  },
+  {
+    "id": 7,
+    "img": "./dist/img/dart_result.jpg",
+    "title": "PSD-макет 'dart service manager'",
+    "link": "pages/dart_service/",
+    "description": "Вёрстка макета из Интернета.",
+    "date": "авг 2019",
+    "tags": "#photoshop #html #css"
+  },
+  {
+    "id": 8,
+    "img": "./dist/img/prototype_result.jpg",
+    "title": "PSD-макет для Softline",
+    "link": "softlinepage/",
+    "description": "Вёрстка макета для компании Softline. Первый проект, написанный на React.",
+    "date": "ноя 2019",
+    "tags": "#photoshop #react"
+  }
+]
