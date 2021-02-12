479f265278201f2ab06f28ae2744f3eb706133da
aleksej <al1111997@yandex.ru>
2021-02-13 02:43:46 +0500

    upd

---
+++ docs/precache-manifest.49a917f76ac05bbc49a229b972cf4611.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118040282149daf288528b9e62cf37d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f1120bc88b1c85e17cb",
+    "url": "/static/css/main.488b306b.chunk.css"
+  },
+  {
+    "revision": "e32323642bc482f8e4f3",
+    "url": "/static/js/2.128597e9.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.128597e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f1120bc88b1c85e17cb",
+    "url": "/static/js/main.7b9ff78f.chunk.js"
+  },
+  {
+    "revision": "be5d5328f9bf7e4d755c",
+    "url": "/static/js/runtime-main.6cb5c86e.js"
+  },
+  {
+    "revision": "e90295bae7e0376c2e231b29ec054ae6",
+    "url": "/static/media/avatar.e90295ba.JPG"
+  }
+]);
